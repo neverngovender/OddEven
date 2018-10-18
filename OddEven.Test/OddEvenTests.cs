8c48329fe00161450199f6dc51ae3293badeb3fc
Nevern Govender <43511671+neverngovender@users.noreply.github.com>
2018-10-18 16:59:04 +0200

    Test and implementation when given a prime number to return number.

--- OddEven.Test/OddEvenTests.cs
+++ OddEven.Test/OddEvenTests.cs
@@ -5,6 +5,7 @@ namespace OddEven.Test
     [TestFixture]
     public class OddEvenTests
     {
+        [TestCase("Odd", 1)]
         [TestCase("Odd", 9)]
         [TestCase("Odd", 15)]
         [TestCase("Odd", 27)]
@@ -17,13 +18,24 @@ namespace OddEven.Test
             Assert.AreEqual(expected, result);
         }
 
-        [TestCase("Even", 2)]
         [TestCase("Even", 4)]
         [TestCase("Even", 6)]
         [TestCase("Even", 8)]
         [TestCase("Even", 10)]
         [TestCase("Even", 12)]
         public void Given_an_even_number_return_string_Even(string expected, int number)
+        {
+            var result = OddEven.Print(number);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("5", 5)]
+        [TestCase("7", 7)]
+        [TestCase("11", 11)]
+        [TestCase("13", 13)]
+        public void Given_a_prime_number_return_number(string expected, int number)
         {
             var result = OddEven.Print(number);
             Assert.AreEqual(expected, result);
--- OddEven/OddEven.cs
+++ OddEven/OddEven.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Globalization;
 
 namespace OddEven
 {
@@ -7,7 +7,25 @@ namespace OddEven
         public static string Print(int number)
         {
             var result = number % 2 == 0 ? "Even" : "Odd";
+
+            if (IsPrime(number))
+                result = number.ToString();
+
             return result;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i < number; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
         }
     }
 }
